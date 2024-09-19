72298eef43d43ad6735b1dc339a330d84b92d940
Hamza Zafar <irexspare@gmail.com>
2024-09-19 16:35:41 +0500

    Hamza: update => add add bootsplash

--- android/app/src/main/java/com/everace/tech/scanner/MainActivity.kt
+++ android/app/src/main/java/com/everace/tech/scanner/MainActivity.kt
@@ -5,6 +5,7 @@ import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash
 
 class MainActivity : ReactActivity() {
 
@@ -15,6 +16,7 @@ class MainActivity : ReactActivity() {
   override fun getMainComponentName(): String = "EverScanner"
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        RNBootSplash.init(this, R.style.BootTheme)
         super.onCreate(null)
     }
 
