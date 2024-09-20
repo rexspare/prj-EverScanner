245d9f9fb0e7b3f6b5c6df3c705d082dfcedbc07
Hamza Zafar <irexspare@gmail.com>
2024-09-20 18:04:02 +0500

    Hamza: update => add update  update header

--- src/components/headers/generateHeader.tsx
+++ src/components/headers/generateHeader.tsx
@@ -1,8 +1,9 @@
-import { StatusBar, StyleSheet, Text, View } from 'react-native'
+import { StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { COLORS, FONT_SIZE, wp } from '../../assets/stylesGuide'
 import { hasNotch, isIOS } from '../../utils/myUtils'
 import Label from '../label'
+import FontAwesome from 'react-native-vector-icons/FontAwesome6'
 
 interface IGenerateHeader {
     title?: string;
@@ -15,6 +16,14 @@ const GenerateHeader = (props: IGenerateHeader) => {
     return (
         <View style={styles.main}>
             <Label style={styles.title}>{title}</Label>
+
+            <TouchableOpacity
+                activeOpacity={0.8}
+                style={styles.btn}
+                hitSlop={{ top: 10, left: 10, bottom: 10, right: 10 }}
+            >
+                <FontAwesome name={"bars-staggered"} color={COLORS.INACTIVE} size={15} />
+            </TouchableOpacity>
         </View>
     )
 }
@@ -25,8 +34,9 @@ const styles = StyleSheet.create({
     main: {
         width: wp(100),
         marginTop: (isIOS() && hasNotch()) ? 60 : StatusBar.currentHeight,
-        justifyContent: 'center',
-        alignItems: 'flex-start',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
         paddingHorizontal: '5%',
         borderBottomWidth: 1 / 3,
         borderColor: COLORS.GREY_40
@@ -35,5 +45,19 @@ const styles = StyleSheet.create({
         textAlign: 'left',
         color: COLORS.INACTIVE,
         fontSize: FONT_SIZE._22
+    },
+    btn: {
+        padding: 6,
+        backgroundColor: COLORS.SECONDARY,
+        shadowColor: "#000000",
+        shadowOffset: {
+            width: 0,
+            height: 9,
+        },
+        shadowOpacity: 0.48,
+        shadowRadius: 11.95,
+
+        elevation: 18,
+        borderRadius: 5
     }
 })
